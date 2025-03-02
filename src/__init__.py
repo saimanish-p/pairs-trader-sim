53ce5e422d941a7e1224f6d81e766ea3a59352c6
Saimanish-P <161119631+saimanish-p@users.noreply.github.com>
2025-03-02 19:42:02 +0400

    Package initialisation

---
+++ src/__init__.py
@@ -0,0 +1,50 @@
+# From styling.py
+from src.styling import render_header
+
+# From charts.py
+from src.charts import plot_pnl, display_results
+
+# From trader.py
+from src.trader import (
+    cointegration_test,
+    calculate_zscore,
+    generate_signal,
+    update_status,
+    calculate_buy_price,
+    calculate_sell_price,
+    calculate_mtm,
+    run_strategy
+)
+
+# From data.py
+from src.data import read_price_data, validate_lookback_period
+
+# From user_inputs.py
+from src.user_inputs import get_params, validate_params
+
+__all__ = [
+    # styling.py
+    'render_header',
+    
+    # charts.py
+    'plot_pnl',
+    'display_results',
+    
+    # trader.py
+    'cointegration_test',
+    'calculate_zscore',
+    'generate_signal',
+    'update_status',
+    'calculate_buy_price',
+    'calculate_sell_price',
+    'calculate_mtm',
+    'run_strategy',
+    
+    # data.py
+    'read_price_data',
+    'validate_lookback_period',
+    
+    # user_inputs.py
+    'get_params',
+    'validate_params'
+]
