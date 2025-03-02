61f65ffb291ab6f5ce740f1fbb19b92eebace1b9
Saimanish-P <161119631+saimanish-p@users.noreply.github.com>
2025-03-02 19:45:48 +0400

    Main page from which streamlit is run to host project

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import logging
+
+from src.styling import render_header
+from src.charts import display_results 
+
+def main():
+
+    logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    # Set page config
+    st.set_page_config(
+        page_icon="📈",
+        layout="wide"
+    )
+
+    render_header()
+
+    display_results()
+
+if __name__ == "__main__":
+    main()
